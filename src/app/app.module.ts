d6bffc8539acd362dc5a34b4eef5dff78286d1c4
fredkendy <fredkendy9@gmail.com>
2023-12-26 11:17:15 -0300

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CalendarModule } from 'primeng/calendar'
+
+//Agrupa componentes, diretivas e serviços
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
